60c1e7bfe5810f593abc984652832a8f902e4ad2
Otsile076 <otsilekatane@gmail.com>
2023-03-29 16:39:24 +0200

    IWA03 challenge 2

---
+++ Interactive Web Applications/challenges/IWA-3/Challenge-2/johannes.js
@@ -0,0 +1,8 @@
+// johannes.js
+
+export const firstname = "Johannes"
+export const surname = "Potgieter"
+export const role = "Intern"
+
+const display= firstname + " " + surname
+document.querySelector('#johannes').innerText = display
