6cd605b5d5d5516ca4700ae514111542f793d4b0
Otsile076 <otsilekatane@gmail.com>
2023-03-30 14:08:27 +0200

    IWA-4

---
+++ Interactive Web Applications/challenges/IWA-4/Challenge-1/script.js
@@ -0,0 +1,33 @@
+//const was used instead of let
+let date = 2050
+let status = 'Parent'
+let count = 0
+ //some monthe were written as "date"
+if (date = 2050) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	console.log("April", 'Family Day','Freedom Day')
+    console.log("May", 'Workers day')
+	count += 5
+
+	if (status == 'true') {
+	  console.log('June', 'Youth Day')
+		let count = count + 1
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	console.log("December", 'Day of Reconciliation', 'Day of Goodwill')
+	count += 4
+
+	if (status == "false") {
+        console.log(date, 'Christmas Day')
+		count += 1
+  }
+
+ 
+}
+
+console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
