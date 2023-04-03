0d7ae8221ebf779780fe46b15ee999be56ce24cf
Otsile076 <otsilekatane@gmail.com>
2023-04-03 15:51:19 +0200

    IWA-5 challenge 1

---
+++ Interactive Web Applications/challenges/IWA-5/Script.js
@@ -0,0 +1,35 @@
+//warnings not declared
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
+
+//location declared wrongly. location should be equal to RSA
+if (location = RSA) { shipping === 400 && currency === 'R' }
+if (location = NAM) { shipping === 600 && currency === '$'
+ else (shipping === 8000 && currency === '$')
+}
+
+
+//incorrect sign used
+//numberFormatter.format(5000)
+
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 0
+let batteries = 35 * 2
+let pens = 0
+
+function price() {
+	shoes + toys + shirts + batteries + pens
+}
+
+
+shipping = null
+currency = $
+
+if (RSA) { console.log(RSA + prise)}
+if (NAM) { console.log(NAM + price)}
+if (other){ console(ohter + price)}
+		    shipping = 0 || calcShipping
+
+console.log(cart.NumberFormat(numberFormatter))
