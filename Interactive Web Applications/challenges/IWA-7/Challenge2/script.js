0eb4cb44c3326af97d59dfc241b42b00e3845b0f
Otsile076 <otsilekatane@gmail.com>
2023-04-05 15:27:37 +0200

    first commit

---
+++ Interactive Web Applications/challenges/IWA-7/Challenge2/script.js
@@ -0,0 +1,15 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if(nickname !== "Timmy") {
+console.log(`Good Morning, ${firstname}`) 
+} else {
+    console.log(`Good Morning, ${nickname}`)
+}
+//backticks weren't placed properly to encompass the relevent code
+ 
+
+
+
+
+
