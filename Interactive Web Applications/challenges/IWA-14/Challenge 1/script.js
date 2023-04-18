8ffd7345efe337f3c798a85a3d82307d4b26fe3d
Otsile076 <otsilekatane@gmail.com>
2023-04-18 20:52:08 +0200

    Initial Commit

---
+++ Interactive Web Applications/challenges/IWA-14/Challenge 1/script.js
@@ -0,0 +1,12 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const result = () => {
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+result()
+
+//console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`)
